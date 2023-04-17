16ad00f2f6090cd547539e8ec75b781b6316dde6
Woliver <woliveraraujo@hotmail.com>
2023-04-17 01:01:58 -0300

    update

--- src/Teste.java
+++ src/Teste.java
@@ -1,4 +1,16 @@
 
 public class Teste {
+	
+	public static void main(String[] args) {
+        Carro carro = new CarroUsado("Toyota", "Corolla", 2020, 120000);
+        Compra compra = new Compra(carro, 80000);
+        Movimento movimento = new Movimento(compra, "01/01/2023");
+
+        System.out.println("Compra realizada em " + movimento.getData());
+        System.out.println("Marca do carro: " + carro.getMarca());
+        System.out.println("Modelo do carro: " + carro.getModelo());
+        System.out.println("Ano do carro: " + carro.getAno());
+        System.out.println("Valor da compra: " + compra.getValor());
+    }
 
 }
